8600ddcc5b4893c7d15ac38387bc4a3dfe27f48a
rasittutgun <47606227+rasittutgun@users.noreply.github.com>
2026-06-29 16:29:33 +0300

    Change default FoV value from 30 to 120 degrees

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.caption("Skyfield + Cartopy based nadir track and antenna footprint visualiza
 with st.sidebar:
     st.header("Input Parameters")
     tle_text = st.text_area("TLE (2 veya 3 satır)", value=ISS_TLE_DEFAULT, height=140)
-    fov_deg = st.number_input("Antenna FoV (derece)", min_value=0.1, max_value=179.0, value=30.0, step=0.5)
+    fov_deg = st.number_input("Antenna FoV (derece)", min_value=0.1, max_value=179.0, value=120.0, step=0.5)
 
     now_utc = datetime.now(timezone.utc)
     default_start = now_utc
